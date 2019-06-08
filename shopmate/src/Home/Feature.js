bdc4c00f6a18a4deddb95dba9896a1c796c96bd0
joye <shonubij@gmail.com>
2019-06-08 22:25:00 +0100

    feature section design

--- shopmate/src/Home/Feature.js
+++ shopmate/src/Home/Feature.js
@@ -2,7 +2,15 @@ import  React from "react"
 
 function Feature(){
     return(
-        <div className ="feature">FEATURE</div>
+        <section className ="feature">
+        <img src= "/images/featuresm.png"/>
+        <div>
+        <h2>Vera Bradley</h2>
+        <p>Carry the day in style with this extra-large tote crafted in our chic B.B.
+         Collection textured PVC. Featuring colorful faux leather trim, this tote offers a roomy interior plus just enough perfectly placed pockets to keep smaller items organized and easy to find.</p>
+         <a className = "btn btn-ghost" href = "#">Shop now</a>
+         </div>
+        </section>
     )
 }
 
